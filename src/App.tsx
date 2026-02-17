a374cd0657559a08c019b10e677ab54d41a1e5e5
Paula <paausj@gmail.com>
2026-02-17 11:25:08 +0100

    feat: initial project setup with Vite, TypeScript, Tailwind and Shadcn

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-sky-400 drop-shadow-lg">
+        ¡VisuWallet con Tailwind v4! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
